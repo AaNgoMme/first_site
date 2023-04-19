b7a23f7758ec625e093ac411ba8742df2cc7d454
AaNgoMme <AaNgoMme@gmail.com>
2023-04-19 23:16:03 +0300

    v0.11

--- script/script2.js
+++ script/script2.js
@@ -4,7 +4,7 @@ let boxImage = document.querySelector(".box1-image1");
 for (let menuImage of menuImageList) {
     menuImage.onclick = function() {
         boxImage.innerHTML = `<img src="${menuImage.src}" class="box1-image">`;
-    }
+    };
 };
 
 let menuImageList2 = document.querySelectorAll(".menu-image-list2");
@@ -13,7 +13,7 @@ let boxImage2 = document.querySelector(".box2-image2");
 for (let menuImage2 of menuImageList2) {
     menuImage2.onclick = function() {
         boxImage2.innerHTML = `<img src="${menuImage2.src}">`;
-    }
+    };
 };
 
 let menuImageList3 = document.querySelectorAll(".menu-image-list3");
@@ -22,7 +22,7 @@ let boxImage3 = document.querySelector(".box3-image3");
 for (let menuImage3 of menuImageList3) {
     menuImage3.onclick = function() {
         boxImage3.innerHTML = `<img src="${menuImage3.src}">`;
-    }
+    };
 };
 
 let menuImageList4 = document.querySelectorAll(".menu-image-list4");
@@ -31,7 +31,7 @@ let boxImage4 = document.querySelector(".box4-image4");
 for (let menuImage4 of menuImageList4) {
     menuImage4.onclick = function() {
         boxImage4.innerHTML = `<img src="${menuImage4.src}">`;
-    }
+    };
 };
 
 
@@ -40,7 +40,7 @@ let ul = document.querySelector(".head-list1");
 
 button.onclick = function() {
     ul.classList.toggle("open");
-}
+};
 
 let buttonMenu = document.querySelector('.button-menu');
 let small = document.querySelector(".small-screen-menu");
@@ -52,7 +52,7 @@ buttonMenu.onclick = function() {
     buttonMenu2.style.display = "block"
     buttonMenu2.style.width = "50px"
     buttonMenu2.style.height = "35px"
-}
+};
 
 buttonMenu2.onclick = function() {
     small.classList.toggle("open");
@@ -60,7 +60,7 @@ buttonMenu2.onclick = function() {
     buttonMenu.style.display = "block"
     buttonMenu.style.width = "50px"
     buttonMenu.style.height = "35px"
-}
+};
 
 function someFunc() {
     let width = window.innerWidth;
@@ -68,44 +68,38 @@ function someFunc() {
         buttonMenu2.style.display = "none";
         buttonMenu.style.display = "none";
     }
-  }
-  window.addEventListener('resize', function() {
+}
+window.addEventListener('resize', function() {
     buttonMenu.style.display = "block";
     someFunc();
-  });
+});
 
 
-  
-
-
-
-
-
-  const slider = document.querySelector('.carousel');
+const slider = document.querySelector('.carousel');
 let isDown = false;
 let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', e => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
 });
 slider.addEventListener('mouseleave', _ => {
-  isDown = false;
-  slider.classList.remove('active');
+    isDown = false;
+    slider.classList.remove('active');
 });
 slider.addEventListener('mouseup', _ => {
-  isDown = false;
-  slider.classList.remove('active');
+    isDown = false;
+    slider.classList.remove('active');
 });
 slider.addEventListener('mousemove', e => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const SCROLL_SPEED = 1;
-  const walk = (x - startX) * SCROLL_SPEED;
-  slider.scrollLeft = scrollLeft - walk;
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const SCROLL_SPEED = 1;
+    const walk = (x - startX) * SCROLL_SPEED;
+    slider.scrollLeft = scrollLeft - walk;
 });
 
